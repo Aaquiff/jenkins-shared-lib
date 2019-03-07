db90e087f75e3b4c0a2615d405b368b35dc75995
Aaquiff Ahnaff Rauf <aaquiff@wso2.com>
2019-03-07 10:33:14 +0530

    Support for WUM update

--- vars/addWumProduct.groovy
+++ vars/addWumProduct.groovy
@@ -18,16 +18,58 @@
 */
 
 def call(Map config) {
-  def productVersion = "${config.PRODUCT}-${config.VERSION}"
+  
+  def WUM = "wum"
+  def MV="mv"
+  def UNZIP="unzip"
+  def RM ="rm"
+
+  def PRODUCT="${config.PRODUCT}"
+  def PRODUCT_VERSION="${config.VERSION}"
+  def CHANNEL="full"
+  def WUM_PRODUCT_HOME="${config.WUM_PRODUCT_HOME}"
+  def PACK_DEST="${config.PACK_DEST}"
+
   sh """
-    wum init -u "${config.WSO2_USERNAME}" -p "${config.WSO2_PASSWORD}"
-    if wum list | grep "${productVersion}"; 
+    ${WUM} init -u "${config.WSO2_USERNAME}" -p "${config.WSO2_PASSWORD}"
+    if ${WUM} list | grep "${PRODUCT}-${PRODUCT_VERSION}"; 
     then 
-        echo "${productVersion} already exists"; 
+        echo "${PRODUCT}-${PRODUCT_VERSION} already exists"; 
     else 
-        wum add "${productVersion}" -y
-        echo "added ${productVersion}"; 
+        ${WUM} add "${PRODUCT}-${PRODUCT_VERSION}" -y
+        echo "added ${PRODUCT}-${PRODUCT_VERSION}"; 
     fi;
-    wum update ${productVersion}
+    
+    echo "Get latest updates for the product - ${PRODUCT}-${PRODUCT_VERSION}..." &>> wum.log
+    ${WUM} update ${PRODUCT}-${PRODUCT_VERSION} ${CHANNEL} &>> wum.log
+    if [ $? -eq 0 ] ; then
+      echo "${PRODUCT}-${PRODUCT_VERSION} successfully updated..." &>> wum.log
+    else
+      if [ $? -eq 1 ] ; then
+        exit 1
+      fi
+    fi
+
+    echo "Moving the WUM updated product..." &>> wum.log
+    ${MV} ${WUM_PRODUCT_HOME}/${PRODUCT}/${PRODUCT_VERSION}/${CHANNEL}/${PRODUCT}-${PRODUCT_VERSION}*.zip ${PACK_DEST}/${PRODUCT}-${PRODUCT_VERSION}.zip
+    if [ $? -ne 0 ] ; then
+      echo "Failed to move the WUM updated product from ${WUM_PRODUCT_HOME}/${PRODUCT}/${PRODUCT_VERSION}/${CHANNEL} to ${PACK_DEST}..."
+      exit 1
+    fi
+
+    echo "Unzip the WUM updated product..." &>> wum.log
+    ${UNZIP} -q ${PACK_DEST}/${PRODUCT}-${PRODUCT_VERSION}.zip -d ${PACK_DEST}/${DEPLOYMENT_PATTERN}/
+    if [ $? -ne 0 ] ; then
+      echo "Failed to unzip the WUM updated product ${PRODUCT}-${PRODUCT_VERSION}..."
+      exit 1
+    fi
+
+    echo "Remove the zipped product..." &>> wum.log
+    ${RM} ${PACK_DEST}/${PRODUCT}-${PRODUCT_VERSION}.zip
+    if [ $? -ne 0 ] ; then
+      echo "Failed to remove the zipped product ${PRODUCT}-${PRODUCT_VERSION}..."
+      exit 1
+    fi
   """
+
 }
